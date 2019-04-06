7f1b56b97138532c5a96c538abf6147369f2a4a9
glen <ssspspso@gmail.com>
2019-04-06 12:28:07 +0800

    add models

--- browser/cesium-vue/src/components/cesiumContainer.vue
+++ browser/cesium-vue/src/components/cesiumContainer.vue
@@ -10,7 +10,36 @@ import widgets from'cesium/Widgets/widgets.css';
 export default {
   name: 'cesiumContainer',
   mounted(){
-  	 var viewer = new Cesium.Viewer('cesiumContainer');
+  	 Cesium.Ion.defaultAccessToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MTNmMWUzMy02OGE1LTQxMDktYTA5OS00NjQwMDE1NmUwMTgiLCJpZCI6ODgzMCwic2NvcGVzIjpbImFzbCIsImFzciIsImFzdyIsImdjIl0sImlhdCI6MTU1MjkwNzkzOH0.mQt1tjUe1fI51n2F_J_sMAurfRFaNouyJ4yFVTXB7pU";
+    var viewer = new Cesium.Viewer("cesiumContainer", {
+      // scene3DOnly: true,
+      // navigationHelpButton: false,
+      terrainProvider: Cesium.createWorldTerrain()
+
+      // imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
+      //    url : 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+      // }),
+      // terrainProvider: Cesium.createWorldTerrain()
+    });
+    // viewer.scene.globe.depthTestAgainstTerrain = true;
+    viewer.scene.globe.enableLighting = true;
+
+    var tileset = new Cesium.Cesium3DTileset({
+      url: Cesium.IonResource.fromAssetId(20119)
+    });
+
+    viewer.scene.primitives.add(tileset);
+    viewer.zoomTo(tileset);
+    var scene = viewer.scene;
+    //   var entity = viewer.entities.add({
+    //       position : Cesium.Cartesian3.fromDegrees(117.713936,39.027913, 0.0),
+    //       model : {
+    //           uri : '/static/models/VC.glb',
+    //           scale: 200
+    //       }
+    //   });
+    //   viewer.trackedEntity = entity;
   }
 }
 </script>
--- browser/cesium-vue/src/main.js
+++ browser/cesium-vue/src/main.js
@@ -13,4 +13,3 @@ new Vue({
   template: '<App/>',
   components: { App } 
 })
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MTNmMWUzMy02OGE1LTQxMDktYTA5OS00NjQwMDE1NmUwMTgiLCJpZCI6ODgzMCwic2NvcGVzIjpbImFzbCIsImFzciIsImFzdyIsImdjIl0sImlhdCI6MTU1MjkwNzkzOH0.mQt1tjUe1fI51n2F_J_sMAurfRFaNouyJ4yFVTXB7pU'
